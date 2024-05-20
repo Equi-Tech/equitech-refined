5db13a8da4eeb3cc41e0f368c5e26eb171138295
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-05-20 22:05:14 +0530

    WIP(static): push script with commitizen validation

--- shpush.sh
+++ shpush.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+
+# Author    : Eshan Roy
+# URI       : https://eshanized.github.io
+
+# NOTE: Valid for Arch Based Linux Distribution
+
+# Function to check if Commitizen is installed
+check_commitizen() {
+    if ! pacman -Qq commitizen-go &> /dev/null; then
+        echo "Commitizen is not installed. Please install it using 'yay -S commitizen-go'." >&2
+        exit 1
+    fi
+}
+
+# Function to stage, commit, and push changes
+push_to_github() {
+    git add .
+    git cz
+    git push origin master
+}
+
+# Main Function
+main() {
+    check_commitizen
+    push_to_github
+}
+
+main
